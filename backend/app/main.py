16f1c8c95c2ecbf2b14ef6d508e0ffdb92763352
Jon Westfall <jon@jonwestfall.com>
2025-07-31 10:23:52 -0500

    Trying to get this up and running on backend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .database import create_db_and_tables
+from app.database import create_db_and_tables
 
 app = FastAPI()
 
