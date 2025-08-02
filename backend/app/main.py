9403f9614797f9edb75f131d3dd3b63c4ce0808e
Jon Westfall <jon@jonwestfall.com>
2025-08-02 18:22:08 -0500

    Include API prefix in Swagger examples

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 from app.routes import (
     users,
     children,
@@ -38,6 +39,23 @@ logging.basicConfig(level=getattr(logging, LOG_LEVEL))
 logger = logging.getLogger(__name__)
 
 app = FastAPI(docs_url=None)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
+    openapi_schema["servers"] = [{"url": "/api"}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 # CORS setup
 app.add_middleware(
